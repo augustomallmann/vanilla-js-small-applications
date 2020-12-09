0193a5898fc8a4fafa8b1741b1ea731ac5d50a33
Augusto Mallmann <augusto@MacBook-Pro-de-Augusto.local>
2020-12-09 14:59:00 -0300

    api update

--- pagespeed/base.js
+++ pagespeed/base.js
@@ -1,16 +1,27 @@
-// var STRATEGIES = ['desktop', 'mobile'];
 const urlInput = document.getElementById('urlInput')
-const strategySelector = document.getElementById('strategySelector')
+const themeSwitcher = document.getElementById('theme-switcher');
+const loader = document.getElementById('loader');
+const ulResults = document.getElementById('results');
 
-let url = '';
-let strategy = '';
 let baseApiUrl = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url='
+let apiKey = 'AIzaSyBRPIT29qvlQ3p7pxuw9XFZVqnzRc-xmII';
+let url = '';
+let strategy = ['mobile','desktop'];
+let results = [];
 
-submitURL = e =>{
-  let url = urlInput.value;
-  let strategy = strategySelector.value;
+
+themeSwitcher.addEventListener('click', ()=>{
+  themeSwitcher.checked ? document.body.classList.add('dark-theme') : document.body.classList.remove('dark-theme')
+})
+
+
+submitURL = (e, strategy) =>{
   e.preventDefault()
+  results = []
+  ulResults.innerHTML = ''
+  let url = urlInput.value;
   
+  loader.classList.remove('d-none');
   try {
     new URL(url);
   } catch (_) {
@@ -18,33 +29,40 @@ submitURL = e =>{
     console.log('url inválida')
     return false;  
   }
-
   urlInput.classList.remove('wrong-url')
   urlInput.classList.add('correct-url')
   
   setTimeout(() =>{
     urlInput.classList.remove('correct-url')
-  }, 1000)
+  }, 1000);
   
-  callApi(url,strategy);
+  strategy.forEach( strategy => {
+    callApi(url,strategy);
+  });
+
   resetFields()
 }
+
 
 resetFields = () =>{
   urlInput.value = '';
 }
 
- callApi = async (url,strategy) =>{
-  let apiUrl = `${baseApiUrl}${url}&strategy=${strategy}`;
+callApi = async (url,strategy) =>{
+  let apiUrl = `${baseApiUrl}${url}&strategy=${strategy}&key=${apiKey}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
-  displayResultsOnScreen(data)
-}
+  loader.classList.add("d-none")
 
-displayResultsOnScreen = (data) => {
-  let fid = data.loadingExperience.metrics.FIRST_INPUT_DELAY_MS.category;
-  let lcp = data.loadingExperience.metrics.LARGEST_CONTENTFUL_PAINT_MS.category;
-  let cls = data.loadingExperience.metrics.CUMULATIVE_LAYOUT_SHIFT_SCORE.category;
+  results.push(data)
 
-console.log(fid,lcp,cls)
+  results.map(result =>{
+    ulResults.insertAdjacentHTML("afterbegin",
+    `<section class="${result.lighthouseResult.configSettings.emulatedFormFactor}-strategy">
+    Your website overall ${result.lighthouseResult.configSettings.emulatedFormFactor} performance is ${result.loadingExperience.overall_category}.
+    </section>
+    ` 
+  )
+
+})
 }
